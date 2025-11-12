594bc7927a6ce8a62227eb3cedecf286fd09aab7
Ziad <ziad.shararaa@gmail.com>
2025-11-12 14:34:57 +0200

    initial commit

---
+++ MySweetProgram/src/MySweetProgram.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class MySweetProgram {
+  public static void main(String[] args) {
+    System.out.println("Enter your name");
+
+    Scanner in = new Scanner(System.in);
+    // String s = in.nextLine();
+
+    /*
+    int i = 0; // initialization
+    while (i < 10) { // condition
+      System.out.println("i is: " + i);
+      i++; // update
+    }
+    */
+
+    /*
+    for (int i = 0; i < 10; i++) {
+      System.out.println(i);
+    }
+    */
+
+    /*
+    int i = 0;
+    do {
+      System.out.println("i is: " + i);
+      i++;
+    } while (i < 10);
+    */
+
+    // in.close();
+  }
+}
